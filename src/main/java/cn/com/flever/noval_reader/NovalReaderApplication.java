a61fd39d1e5d2ced86acba1e644a7e0f2b9de201
fj <392431041@qq.com>
2019-03-29 22:41:45 +0800

    首次提交

---
+++ src/main/java/cn/com/flever/noval_reader/NovalReaderApplication.java
@@ -0,0 +1,13 @@
+package cn.com.flever.noval_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovalReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovalReaderApplication.class, args);
+	}
+
+}
